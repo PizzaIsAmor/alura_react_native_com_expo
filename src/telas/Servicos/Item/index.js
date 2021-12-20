cecf62f4c0471f9c4b7c4b1f282f762f42b6e2b9
Rafael Fernandes Borges <rafaeelfernandes@hotmail.com>
2021-12-20 13:03:34 -0300

    aula 2.3

---
+++ src/telas/Servicos/Item/index.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import {Button, Text, View} from "react-native";
+import estilos from "./estilos";
+import CampoInteiro from "../../../componentes/CampoInteiro";
+
+export default function Item({nome, preco, descricao}) {
+    const [quantidade, setQuantidade] = useState(1);
+
+    return <>
+        <View style={estilos.informacao}>
+            <Text style={estilos.nome}>{nome}</Text>
+            <Text style={estilos.descricao}>{descricao}</Text>
+            <Text style={estilos.preco}>{preco}</Text>
+        </View>
+        <View style={estilos.carrinho}>
+            <View>
+                <View style={estilos.valor}>
+                    <Text style={estilos.descricao}>Quantidade:</Text>
+                    <CampoInteiro valor={quantidade} acao={setQuantidade}/>
+                </View>
+                <View style={estilos.valor}>
+                    <Text style={estilos.descricao}>Preco:</Text>
+                    <Text style={estilos.preco}>0</Text>
+                </View>
+            </View>
+            <Button title="Adicionar"/>
+        </View>
+        <View style={estilos.divisor} />
+    </>
+}
